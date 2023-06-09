9657e4a2804848f0633137d635e575f862b43449
Rakan007 <127819573+Rakan007@users.noreply.github.com>
2023-06-09 23:38:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
-set -cu
+set -eu
 
 export PYTHONUNBUFFERED=true
 
-VIRTUALENV=".data/venv"
+VIRTUALENV=.data/venv
 
-if [ ! -d "$VIRTUALENV" ]; then
-  python3 -m venv "$VIRTUALENV"
+if [ ! -d $VIRTUALENV ]; then
+  python3 -m venv $VIRTUALENV
 fi
 
-if [ ! -f "$VIRTUALENV/bin/pip" ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | "$VIRTUALENV/bin/python"
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
-"$VIRTUALENV/bin/pip" install -r requirements.txt
+$VIRTUALENV/bin/pip install -r requirements.txt
 
-"$VIRTUALENV/bin/python" app.py
+$VIRTUALENV/bin/python3 app.py
 Footer
