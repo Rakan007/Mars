8c01f308522dabd7707941782033ce17ff9cd1d9
Rakan007 <rakandanendrad@gmail.com>
2023-06-10 00:14:38 +0700

    First Commit

--- start.sh
+++ start.sh
@@ -1,26 +1,18 @@
-set -cu
+set -eu
 
+export PYTHONUNBUFFERED=true
 
-export PYTHONUNBUFFERED-truc
-
-
-VIRTUALENV-.data/venc
-
+VIRTUALENV=.data/venv
 
 if [ ! -d $VIRTUALENV ]; then
-
   python3 -m venv $VIRTUALENV
-
 fi
 
 if [ ! -f $VIRTUALENV/bin/pip ]; then
-
-   curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
-
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
-$VIRTUALENV/bin/pip install requirements.txt
+$VIRTUALENV/bin/pip install -r requirements.txt
 
 $VIRTUALENV/bin/python3 app.py
-
 Footer
